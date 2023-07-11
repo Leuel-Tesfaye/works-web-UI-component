9bda4e31757317a16a87b405837fb6768c679c0a
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2023-07-11 09:29:23 +0300

    changes in works js slider

--- js/main.js
+++ js/main.js
@@ -45,33 +45,7 @@ $(function() {
     transitionDuration: '.6s',
   });
 
-  // slider testimonials
-  var swiper = new Swiper('.art-testimonial-slider', {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    speed: 1400,
-    autoplay: false,
-    autoplaySpeed: 5000,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.art-testi-swiper-next',
-      prevEl: '.art-testi-swiper-prev',
-    },
-    breakpoints: {
-      1500: {
-        slidesPerView: 2,
-      },
-      1200: {
-        slidesPerView: 2,
-      },
-      992: {
-        slidesPerView: 1,
-      },
-    },
-  });
+  
 
   // slider works
   var swiper = new Swiper('.art-works-slider', {
